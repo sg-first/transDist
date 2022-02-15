fc59f08aabb658ab0daee3df656bfc180489c8e9
3A <524019351@qq.com>
2022-02-15 16:31:09 +0800

    可以指定新生成分布的偏移量

--- transDist.py
+++ transDist.py
@@ -1,25 +1,25 @@
 import numpy as np
 import scipy.stats as stat
 
-def getNormVal(mean, sigma2, toMovedX, size, sampleNum):
+def getNormVal(mean, sigma2, size, sampleNum, newDistMaxX):
     result=[]
+    toMovedX = lambda x: x - newDistMaxX
 
     lastVal=toMovedX(0)
     i=1
     while i<=size:
         iVal=toMovedX(i)
-        print(iVal)
         cdfSub=stat.norm.cdf(iVal,loc=mean,scale=sigma2)-stat.norm.cdf(lastVal,loc=mean,scale=sigma2)
         result.append(cdfSub*sampleNum)
         lastVal=iVal
         i+=1
     return result
 
-def toNorm(dist):
+def toNorm(dist,newDistMaxX=None):
     size=len(dist)
     dist=np.array(dist)
     maxX=np.argmax(dist) # X轴向左平移多少
-    toMovedX=lambda x:x-maxX
+    toMovedX = lambda x: x - maxX
     sampleNum=np.sum(dist)
     # 均值
     mean=0
@@ -32,7 +32,9 @@ def toNorm(dist):
         sigma2+=dist[i]*(toMovedX(i)-mean)**2
     sigma2/=sampleNum
 
-    return getNormVal(mean,sigma2,toMovedX,size,sampleNum)
+    if newDistMaxX is None:
+        newDistMaxX=maxX
+    return getNormVal(mean,sigma2,size,sampleNum,newDistMaxX)
 
 testList=[1,2,3,2,1]
-print(toNorm(testList))
+print(toNorm(testList,1))
