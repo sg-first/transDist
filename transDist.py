7b1f754f5748187965be1ae844f8f30f5187b2ed
3A <524019351@qq.com>
2022-02-16 17:28:33 +0800

    转均匀分布

--- transDist.py
+++ transDist.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.stats as stat
+from scipy.optimize import fsolve
 
 def getNormVal(mean, sigma2, size, sampleNum, newDistMaxX):
     result=[]
@@ -13,7 +14,7 @@ def getNormVal(mean, sigma2, size, sampleNum, newDistMaxX):
         result.append(cdfSub*sampleNum)
         lastVal=iVal
         i+=1
-    return result
+    return np.array(result)
 
 def toNorm(dist,newDistMaxX=None):
     size=len(dist)
@@ -36,5 +37,26 @@ def toNorm(dist,newDistMaxX=None):
         newDistMaxX=maxX
     return getNormVal(mean,sigma2,size,sampleNum,newDistMaxX)
 
+
+def toUniform(dist2,newDistMaxX=None):
+    n=len(dist2)
+    dist2 = np.array(dist2)
+    avg = np.average(dist2)
+    var = np.var(dist2)
+
+    def equ(i):
+        x=i[0]
+        y=i[1]
+        return [(x + (n - 1) * y)/n - avg,
+                ((((n - 1) * (y-avg)**2) + (x-avg)**2) / n) - var]
+
+    r = fsolve(equ, np.array([0, 0]))
+
+    if newDistMaxX is None:
+        newDistMaxX=np.argmax(dist2)
+    dist2_true_norm = np.full((n,), r[1])
+    dist2_true_norm[newDistMaxX] = r[0]
+    return dist2_true_norm
+
 testList=[1,2,3,2,1]
-print(toNorm(testList,1))
+print(toUniform(testList,1))
